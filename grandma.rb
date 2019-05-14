95581aec281ae48bfea41e196597ee46839aa5a6
Aaron B <abriscoe2008@gmail.com>
2019-05-14 22:13:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,3 +6,16 @@ def speak_to_grandma(phrase)
   else 
     return "NO, NOT SINCE 1938!"
 end 
+
+def speak_to_grandma(phrase)
+  elsif phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+    
+    
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
